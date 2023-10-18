3df225db96096c505d240535285544f663c451a9
fedor <uasily.lol@yandex.ru>
2023-10-18 19:51:52 +0300

    changing the font for page Course Description

--- src/components/fonts/desktop/1920_h5.jsx
+++ src/components/fonts/desktop/1920_h5.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styled from 'styled-components'
 
+    
+export default function StyledText({ children, gray = null }) {
     const SpanStyled = styled.span`
         font-family: "Montserrat-Medium-500";
         font-size: 28px;
         font-style: normal;
         font-weight: 500;
         line-height: 120%;
-        color: #1C1C1C;
+        color: ${() => (gray === true) ? "#757575" : "#1C1C1C"};
         font-variant-numeric: lining-nums tabular-nums;
         font-feature-settings: 'case' on;
     `
-    
-export default function StyledText({ children }) {
     return (
         <SpanStyled>
             {children}
