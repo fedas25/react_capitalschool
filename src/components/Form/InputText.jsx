69d52a379a773c1d9d97f1ce4ad9aac1b4165a19
fedor <uasily.lol@yandex.ru>
2023-10-19 18:55:21 +0300

    создал компонент Input

---
+++ src/components/Form/InputText.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+width: 455px;
+padding: 14px 28px 16px 28px;
+border-radius: 30px;
+border: none;
+background-color: #EFEEF4;
+font-family: "Montserrat-Medium-500";
+font-size: 24px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+color: #BCBCC5;
+outline:none;
+`;
+
+export default function InputText({placeholder, password, value }) {
+  return <Input placeholder={placeholder} value={value} type={(+password) ? "password" : "text"} />
+}
