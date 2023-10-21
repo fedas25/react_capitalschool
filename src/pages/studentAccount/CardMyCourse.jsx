5c329211b9f87c2658f1bd1eaf4b15398e05bb8d
fedor <uasily.lol@yandex.ru>
2023-10-21 23:29:54 +0300

    added WorkingAreaPassed

--- src/pages/studentAccount/CardMyCourse.jsx
+++ src/pages/studentAccount/CardMyCourse.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import H4 from "./../../components/fonts/desktop/1920_h4"
 import H5 from "./../../components/fonts/desktop/1920_h5"
 import H6 from "./../../components/fonts/desktop/1920_h6"
+import BTN from "./../../components/fonts/desktop/1920_button"
 import teacher from "./../../assets/teacher.jpg";
 import Button from '../../components/Button'
 import PricePerHour from '../../components/PricePerHour'
 import InformationAboutCourses from './InformationAboutCourses'
 import ContainerCardCourse from './ContainerCourse'
+import jackdaw from "./../../assets/jackdaw.png";
 
 function Statistics({ className, description, count }) {
     return (
@@ -45,6 +48,8 @@ const Action = styled.div`
 const Main = styled.div`
     width: 100%;
     display: flex;
+    ${(props) => props.type === "passed" ? "justify-content: space-between;" : null}
+    ${(props) => props.type != "passed" ? "gap: 12px;" : null}
     gap: 12px;
 `;
 
@@ -54,25 +59,105 @@ const Secondary = styled.div`
     justify-content: space-between;
 `;
 
-export default function CardMyCourse() {
+const Img = styled.img`
+    height: 64px;
+    width: 64px;
+    margin-right: 32px;
+`;
+
+const Date = styled.div`
+    display: flex;
+    gap: 12px;
+    margin-right: 24px;
+`;
+
+function TypeRecord({ trial }) {
+    return (
+        <>
+            {trial ?
+                <H6>пробное занятие</H6> :
+                <H6>учебное занятие</H6>}
+        </>
+    )
+}
+
+const LinkToSession = styled.div`
+background: #EFEEF4;
+padding: 16px 28px;
+border-radius: 30px;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const InfoContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+function InformationAboutRecord({ className, passed }) {
+    return (
+        <div className={className}>
+            {passed ? (
+                <Img src={jackdaw} alt='jackdaw' />
+            ) : null}
+            <InfoContainer>
+                <Date>
+                    <H4 violet>13 марта</H4>
+                    <H4 violet>16:00</H4>
+                </Date>
+                <TypeRecord trial />
+            </InfoContainer>
+            {passed ? null : (
+                <LinkToSession>
+                    <BTN color="violet">ссылка на трансляцию</BTN>
+                </LinkToSession>
+            )}
+        </div>
+    )
+}
+
+const StyledInformationAboutRecord = styled(InformationAboutRecord)`
+    width: 100%;
+    display: flex;
+    ${(props) => props.record ? "justify-content: space-between;" : null}
+    align-items: center;
+`;
+
+export default function CardMyCourse({ type = null }) {
     return (
         <ContainerCardCourse>
-            <Main>
+            {type === "record" ? <StyledInformationAboutRecord record={+true}/> :
+            type === "passed" ? <StyledInformationAboutRecord passed /> :
+            null}
+            <Main type={type}>
                 <InformationAboutCourses
                     teacherName="Куликова"
                     nameCourse="Losos"
                     colorCourse="#59C4E5"
                     srcTeacher={teacher}
                 />
-                <Action>
-                    <Button title="Оплатить" />
-                    <Button title="Записаться" color="violet" />
-                </Action>
+
+                {type == null ? (
+                    <Action>
+                        <Button title="Оплатить" />
+                        <Button title="Записаться" color="violet" />
+                    </Action>
+                ) : null}
+                {type === "passed" ? (
+                    <Action>
+                        <Button title="Записаться" />
+                    </Action>
+                ) : null}
+
             </Main>
-            <Secondary>
-                <PricePerHour />
-                <StyledPassingStatistics />
-            </Secondary>
+
+            {type ? null : (
+                <Secondary>
+                    <PricePerHour />
+                    <StyledPassingStatistics />
+                </Secondary>
+            )}
         </ContainerCardCourse>
     )
 }
---
+++ src/pages/studentAccount/WorkingAreaPassed/index.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import styled from 'styled-components'
+import NavBarUserAccount from "../../../components/layout/navigation/NavBarUserAccount"
+import Calendar from '../Calendar'
+import CardMyCourse from '../CardMyCourse'
+import sorting from "../../../assets/sorting.svg"
+import FontButton from "./../../../components/fonts/desktop/1920_button"
+
+
+const Container = styled.div`
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+`;
+
+const Content = styled.div`
+    width: 1664px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 96px;
+`;
+
+const ContainerContent = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+    gap: 64px;
+`;
+
+const ListCardAccount = styled.div`
+    width: 1116px;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+`;
+
+const ListCard = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+
+
+const Img = styled.img`
+    height: 48px;
+    width: 48px;
+`;
+
+const SortTime = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+`;
+
+const Type = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 24px;
+`;
+
+const Radio = styled.input`
+    transform: scale(2.85);
+`;
+
+const Radiocell = styled.div`
+    width: 220px;
+    display: flex;
+    align-items: center;
+    gap: 28px;
+`;
+
+
+function ManagingCourses({ className }) {
+    return (
+        <div className={className}>
+            <SortTime >
+                <Img src={sorting} alt="" />
+                <FontButton color="black">Сначала старые</FontButton>
+            </SortTime>
+            <Type>
+                <Radiocell>
+                    <Radio type="checkbox"/>
+                    <FontButton color="black">Учебное занятие</FontButton>
+                </Radiocell>
+                <Radiocell>
+                    <Radio  type="checkbox"/>
+                    <FontButton color="black">Пробное занятие</FontButton>
+                </Radiocell>
+            </Type>
+        </div>
+    )
+}
+
+const StyledManagingCourses = styled(ManagingCourses)`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 12px;
+`;
+
+export default function PersonalData() {
+    return (
+        <Container>
+            <Content>
+                <NavBarUserAccount />
+                <ContainerContent>
+                    <ListCardAccount>
+                        <StyledManagingCourses />
+                        <ListCard>
+                            <CardMyCourse type="passed"/>
+                            <CardMyCourse type="passed"/>
+                        </ListCard>
+                    </ListCardAccount>
+                    <Calendar />
+                </ContainerContent>
+            </Content>
+        </Container>
+    )
+}
