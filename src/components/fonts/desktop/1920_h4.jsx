706724e43d7a6fdab388b419148fdf1facc4ed55
fedor <uasily.lol@yandex.ru>
2023-10-21 20:24:24 +0300

    added purple color to the font

--- src/components/fonts/desktop/1920_h4.jsx
+++ src/components/fonts/desktop/1920_h4.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 
-export default function StyledText({ children, black }) {
+export default function StyledText({ children, black, violet  }) {
     const SpanStyled = styled.span`
         font-family: "Montserrat-Medium-500";
         font-size: 37px;
@@ -9,7 +9,8 @@ export default function StyledText({ children, black }) {
         font-weight: 500;
         line-height: 120%;
         color: #757575;
-        color: ${() => (black === true) ? "#1C1C1C" : "#757575"};
+        color: ${() => (black === true) ? "#1C1C1C" :
+                        (violet === true) ? "#6B73C2" : "#757575"};
         font-variant-numeric: lining-nums tabular-nums;
         font-feature-settings: 'case' on;
     `
