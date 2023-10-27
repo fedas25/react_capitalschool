f9180d2fccebce3d26c3232f51f8387531a95023
fedor <uasily.lol@yandex.ru>
2023-10-27 18:46:57 +0300

    moved creation styles for function in 1920_h6

--- src/components/fonts/desktop/1920_h6.jsx
+++ src/components/fonts/desktop/1920_h6.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styled from 'styled-components'
 
-export default function StyledText({ children , white = null}) {
-    const SpanStyled = styled.span`
-        font-family: "Montserrat-Medium-500";
-        font-size: 21px;
-        font-style: normal;
-        font-weight: 500;
-        line-height: 100%;
-        color: ${() => (white === true) ? "#FFF" : "#757575"};
-    `
-    
+const SpanStyled = styled.span`
+    font-family: "Montserrat-Medium-500";
+    font-size: 21px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 100%;
+    color: ${(props) => (props.white == true) ? "#FFF" : "#757575"};
+`
+
+export default function StyledText({ children, white = null }) {
     return (
-        <SpanStyled>
+        <SpanStyled  white={+white}>
             {children}
         </SpanStyled>
     );
