ba9ee1ba9d4da53ed5cd679b8bc6d96a321f9e66
fedor <uasily.lol@yandex.ru>
2023-10-27 18:46:19 +0300

    moved creation styles for function in 1920_button

--- src/components/fonts/desktop/1920_button.jsx
+++ src/components/fonts/desktop/1920_button.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'styled-components'
 
-export default function StyledText({ children, color }) {
     const SpanStyled = styled.span`
         font-family: "Montserrat-Medium-500";
         font-size: 24px;
         font-style: normal;
         font-weight: 500;
         line-height: normal;
-        color: ${() => (color === "gray") ? "#757575" :
-                        (color === "violet") ? "#6B73C2" :
-                        (color === "black") ? "#1C1C1C" : "#FFF"};
+        color: ${(props) => (props.color === "gray") ? "#757575" :
+                            (props.color === "violet") ? "#6B73C2" :
+                            (props.color === "black") ? "#1C1C1C" : "#FFF"};
     `
 
+export default function StyledText({ children, color }) {
+
     return (
-        <SpanStyled>
+        <SpanStyled color={color}>
             {children}
         </SpanStyled>
     );
