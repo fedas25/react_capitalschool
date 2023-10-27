8d10b211e5d8ce7724a7f453cfc4fbdb4f075418
fedor <uasily.lol@yandex.ru>
2023-10-27 18:46:38 +0300

    moved creation styles for function in 1920_h5

--- src/components/fonts/desktop/1920_h5.jsx
+++ src/components/fonts/desktop/1920_h5.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import styled from 'styled-components'
 
     
-export default function StyledText({ children, gray = null, violet = null }) {
     const SpanStyled = styled.span`
         font-family: "Montserrat-Medium-500";
         font-size: 28px;
         font-style: normal;
         font-weight: 500;
         line-height: 120%;
-        color: ${() => (gray === true) ? "#757575" : (violet === true) ? "#6B73C2" : "#1C1C1C"};
+        color: ${(props) => (props.gray == true) ? "#757575" :
+                            (props.violet === true) ? "#6B73C2" : "#1C1C1C"};
         font-variant-numeric: lining-nums tabular-nums;
         font-feature-settings: 'case' on;
     `
+
+export default function StyledText({ children, gray = null, violet = null }) {
     return (
-        <SpanStyled>
+        <SpanStyled gray={+gray} violet={+violet}>
             {children}
         </SpanStyled>
     );
