ed97a311980e151ccb3c3c89013613004e5bc082
fedor <uasily.lol@yandex.ru>
2023-10-31 23:04:23 +0300

    adding react-spring/web

--- src/components/DropDownListCourses.jsx
+++ src/components/DropDownListCourses.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import H4 from "./fonts/desktop/1920_h4"
+import { useSpring, animated } from '@react-spring/web';
 
-export default function DropDownListCourses() {
-    const ContainerlistCourses = styled.div`
-    position: absolute;
-    width: 100%;
-    padding: 96px 128px 120px 96px;
-    display: flex;
-    justify-content: end;
-    background-color: #6B73C2;
-    z-index: 1;
+const ContainerlistCourses = styled.div`
+position: absolute;
+width: 100%;
+padding: 96px 128px 120px 96px;
+display: flex;
+justify-content: end;
+background-color: #6B73C2;
+z-index: 1;
+top: 50px;
 `;
-    const List = styled.div`
-    min-width: 100px;
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
+const List = styled.div`
+min-width: 100px;
+display: flex;
+flex-direction: column;
+gap: 24px;
 `;
+
+const AnimatedContainerlistCourses = animated(ContainerlistCourses)
+
+export default function DropDownListCourses({setHandleAnimation}) {
+
+
+    // setHandleAnimation(123)
+
+
+
+    const [show, setShow] = useState(false);
+
+    const [springs, api] = useSpring(() => ({
+      from: { y: 0 },
+    }))
+  
+    api.start({
+      from: {
+        y: show ? 0 : 100,
+      },
+      to: {
+        y: show ? 100 : 0,
+      },
+    })
+    
+    const animationHandler = () => {
+      setShow(!show)
+      console.log(show);
+    }
 
     return (
-        <ContainerlistCourses>
+        <AnimatedContainerlistCourses style={springs}>
             <List>
                 <H4 white>A1 (Beginner Level)</H4>
                 <H4 white>A2 (Elementary Level)</H4>
@@ -30,7 +60,13 @@ export default function DropDownListCourses() {
                 <H4 white>B2 (Upper-Intermediate Level)</H4>
                 <H4 white>C1 (Intermediate Level)</H4>
                 <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <H4 white>C2 (Upper-Intermediate Level)</H4>
+                <button onClick={animationHandler}>LOSOS!</button>
             </List>
-        </ContainerlistCourses>
+        </AnimatedContainerlistCourses>
     )
 }
