862e15b2a215be36ccae12079a008c5cd9b3af2c
fedor <uasily.lol@yandex.ru>
2023-11-02 20:44:40 +0300

    took out creation of styles componrnt 1920_h2

--- src/components/fonts/desktop/1920_h2.jsx
+++ src/components/fonts/desktop/1920_h2.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components'
 
-export default function StyledText({ children }) {
-    const SpanStyled = styled.span`
-        font-family: "Montserrat-Medium-500";
-        font-size: 65px;
-        font-style: normal;
-        font-weight: 500;
-        line-height: 100%;
-        color: #1C1C1C;
-    `
+const SpanStyled = styled.span`
+    font-family: "Montserrat-Medium-500";
+    font-size: 65px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 100%;
+    color: #1C1C1C;
+`
 
+export default function StyledText({ children }) {
     return (
         <SpanStyled>
             {children}
