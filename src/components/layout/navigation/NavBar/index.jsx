0b14bb2f6b1e4b4aaa803630a74f1ac56e4d0e55
fedor <uasily.lol@yandex.ru>
2023-11-02 20:58:06 +0300

    added drop-down window of courses in NavBar

--- src/components/layout/navigation/NavBar/index.jsx
+++ src/components/layout/navigation/NavBar/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import { useSpring, animated } from '@react-spring/web';
 import logo from './../../../../assets/logo.svg';
 import Menu from "./menuItems";
+import H4 from "./../../../fonts/desktop/1920_h4"
 
 const Contaier = styled.div`
     display: flex;
@@ -13,9 +15,29 @@ const Contaier = styled.div`
     border-bottom: 2px solid rgba(255, 255, 255, 0.50);
 `;
 
-function Logo({className}) {
-    return(
-        <img className={className} src={logo} alt='logo'/>
+const ContainerlistCourses = styled.div`
+    position: absolute;
+    width: 100%;
+    padding: 96px 128px 120px 96px;
+    display: flex;
+    justify-content: end;
+    background-color: #6B73C2;
+    z-index: 1;
+    top: -1400px;
+`;
+
+const List = styled.div`
+    min-width: 100px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`;
+
+const AnimatedContainerlistCourses = animated(ContainerlistCourses)
+
+function Logo({ className }) {
+    return (
+        <img className={className} src={logo} alt='logo' />
     )
 }
 
@@ -24,11 +46,43 @@ const StyledLogo = styled(Logo)`
     width: 118px;
 `;
 
-export default function NavBar({ handle }) {
+export default function NavBar({ courses }) {
+
+    const [show, setShow] = useState(1);
+    const [springs, api] = useSpring(() => ({}))
+    
+    api.start({
+        from: {
+            y: (show === 1) ? 0 : show ? 0 : 1552,
+        },
+        to: {
+            y: (show === 1) ? 0 : show ? 1552 : 0,
+        },
+    })
+    
+    function animationHandler() {
+        if (show === 1) {
+            setShow(true)
+        } else setShow(!show)
+    }
+
+    const listCourses = courses.map((course, i) =>
+        <H4 key={i} white>
+            {course}
+        </H4>
+      );
+
     return (
-        <Contaier>
-            <StyledLogo />
-            <Menu handle={handle}/>
-        </Contaier>
+        <>
+            <Contaier>
+                <StyledLogo />
+                <Menu handle={animationHandler} />
+            </Contaier>
+            <AnimatedContainerlistCourses style={springs}>
+                <List>
+                    {listCourses}
+                </List>
+            </AnimatedContainerlistCourses>
+        </>
     );
 }
