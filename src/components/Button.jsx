63bb1a3c0f1f3e5f405f490097c89bb56cb7f0c3
fedor <uasily.lol@yandex.ru>
2023-11-03 21:43:32 +0300

    fixed a bug with font redrawing during animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import styled from 'styled-components'
 import FontButton from './fonts/desktop/1920_button'
 
-export default function Button({ title, color, textColor}) {
+const Button = styled.div`
+    display: inline-flex;
+    padding: 18px 28px 20px 28px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 50px;
+    cursor: pointer;
+    background: ${(props) => {
+            return (props.color === "violet" ? "#6B73C2" :
+            props.color === "gray" ? "rgba(107, 115, 194, 0.10);" : "#000")
+            }};
+    `;
 
-    const Button = styled.div`
-        display: inline-flex;
-        padding: 18px 28px 20px 28px;
-        justify-content: center;
-        align-items: center;
-        gap: 10px;
-        border-radius: 50px;
-        cursor: pointer;
-        background: ${() => {
-                return (color === "violet" ? "#6B73C2" :
-                        color === "gray" ? "rgba(107, 115, 194, 0.10);" : "#000")
-                }};
-        `;
-
+export default function Main({ title, color = null, textColor}) {
     return (
         <Button>
-            <FontButton color={textColor}>{title}</FontButton>
+            <FontButton color={color}>{title}</FontButton>
         </Button>
     );
 }
